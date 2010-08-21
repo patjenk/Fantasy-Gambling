f357e6b5db42cda477f5e0ef0b95629d8c1fe35f
Daniel Mican <dm03514@gmail.com>
2010-08-21 21:06:17 +0000

    Added default view, authenticated login, added models

---
+++ Fantasy_Gambling/bets/admin.py
@@ -0,0 +1,13 @@
+from Fantasy_Gambling.bets.models import Sport, League, Season, SeasonInterval, Event, BettingWebsite, BetType, Bet, BetLine 
+from django.contrib import admin
+
+admin.site.register(Sport)
+admin.site.register(League)
+admin.site.register(Season)
+admin.site.register(SeasonInterval)
+admin.site.register(Event)
+admin.site.register(BettingWebsite)
+admin.site.register(BetType)
+admin.site.register(Bet)
+admin.site.register(BetLine)
+
--- Fantasy_Gambling/urls.py
+++ Fantasy_Gambling/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    
+    (r'.*', 'Fantasy_Gambling.bets.views.index'),
+    
 )
